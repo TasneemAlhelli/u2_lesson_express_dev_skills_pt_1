c4ee864571568ea90e1362fc87dd0113016de4dd
Tasneem <tasneem.jameel9@gmail.com>
2024-01-30 08:59:53 +0300

    complete express dev skills

--- express-dev-skills/controllers/skills.js
+++ express-dev-skills/controllers/skills.js
@@ -1,10 +1,18 @@
-const skill = require('../models/skill')
+const skillModel = require('../models/skill')
 
 const index = (req, res) => {
-  const skills = skill.getAll()
+  const skills = skillModel.getAll()
   res.render('skills/index', { skills })
 }
 
+const show = (req, res) => {
+  const id = req.params.id
+  const skill = skillModel.getOne(id)
+  console.log(id)
+  res.render('skills/show', { skill })
+}
+
 module.exports = {
-  index
+  index,
+  show
 }
--- express-dev-skills/models/skill.js
+++ express-dev-skills/models/skill.js
@@ -1,29 +1,37 @@
+const skills = [
+  {
+    id: 1,
+    name: 'HTML'
+  },
+  {
+    id: 2,
+    name: 'CSS'
+  },
+  {
+    id: 3,
+    name: 'JavaScript'
+  },
+  {
+    id: 4,
+    name: 'Node.js'
+  },
+  {
+    id: 5,
+    name: 'React.js'
+  }
+]
+
 const getAll = () => {
-  const skills = [
-    {
-      id: 1,
-      name: 'HTML'
-    },
-    {
-      id: 2,
-      name: 'CSS'
-    },
-    {
-      id: 3,
-      name: 'JavaScript'
-    },
-    {
-      id: 4,
-      name: 'Node.js'
-    },
-    {
-      id: 5,
-      name: 'React.js'
-    }
-  ]
   return skills
 }
 
+const getOne = (id) => {
+  return skills.find((skill) => {
+    return skill.id === parseInt(id)
+  })
+}
+
 module.exports = {
-  getAll
+  getAll,
+  getOne
 }
